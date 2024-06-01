6b8b141753c9197cf9ceedbf709aa0b683f36a25
Mariamikob <mariam.kobiashvili.1@btu.edu.ge>
2024-06-01 10:35:07 +0400

    firsthomework

---
+++ ProjectSuite2/TestCompleteHomework/Script/OrderTests.js
@@ -0,0 +1,13 @@
+﻿var orderAppSteps = require("OrderAppSteps");
+var otherOrderAppSteps = require("OtherOrderAppSteps")
+
+function test1(){
+  orderAppSteps.openApp();
+  
+    otherOrderAppSteps.assertionThatIsVisible()
+    .openOrderForm().addOrder1().clickOkButton()
+    .openOrderForm().addOrder2().clickOkButton()
+    .openOrderForm().addOrder3().clickOkButton();
+     orderAppSteps.closeApp();
+     otherOrderAppSteps.checkingThatWindowIsClosed();
+}
